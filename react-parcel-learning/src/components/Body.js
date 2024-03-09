5ca33de3743c3ac0d8f22e5f85eb2e3e96d8760f
shravan20 <mrshravankumarb@gmail.com>
2024-03-10 04:33:07 +0530

    feat: add search functionality

--- react-parcel-learning/src/components/Body.js
+++ react-parcel-learning/src/components/Body.js
@@ -7,13 +7,38 @@ export default Body = () => {
     // Normal JS Variable
     // let items = restaurants;
 
-    // State Variable
+    // State Variable: Whenever state variables update, React triggers a reconciliation cycle (re-render)
     const [items, setItems] = useState(restaurants);
+    const [searchText, setSearchText] = useState(null);
+
+    function searchTextOnType(e) {
+        console.log("Current State =>" + searchText)
+
+        let data = items.filter(item => {
+            let name = item.data.name.toLowerCase();
+            return name.includes(searchText.toLowerCase())
+        });
+        setItems(data);
+
+        // console.log(data)
+
+    }
 
 
     return (
         <div className="body">
             <div className="filter">
+                <div className="search">
+                    <input
+                        type="text"
+                        className="search-box"
+                        onChange={e => { setSearchText(e.target.value) }}
+                    />
+                    <button
+                        onClick={e => searchTextOnType(e)}
+                    >Search</button>
+
+                </div>
                 <button
                     className="filter-btn"
                     onMouseEnter={() => {
@@ -35,3 +60,4 @@ export default Body = () => {
         </div>
     );
 };
+
